d2da95e90c35907082e114ad7d3cb25ff83f5482
swastikaswast <60318167+swastikaswast@users.noreply.github.com>
2021-04-18 18:14:37 +0000

    Deploying to gh-pages from master @ 1815b9bac8057ca475126a49a5bde4f9558ee670 🚀

--- swast.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="swast" tilewidth="32" tileheight="32" tilecount="180" columns="18">
- <image source="../../../Downloads/ezgif.com-gif-maker.gif" width="600" height="338"/>
-</tileset>
